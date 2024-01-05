b86f9f1a447f240c32e8e933a66e9d3968bed4d7
MathildeLekens <mathilde.lekens@epitech.eu>
2024-01-05 17:21:27 +0100

    [+] Added my_put_nbr.c : File used to display a number

---
+++ lib/my/my_put_nbr.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPE-110-LIL-1-1-secured-mathilde.lekens
+** File description:
+** my_put_nbr.c
+*/
+
+#include "../../include/hashtable.h"
+
+int my_put_nbr(int nb)
+{
+    long int nb2 = nb;
+    if (nb2 < 0) {
+        nb2 *= -1;
+        my_putchar('-');
+    }
+    if (nb2 >= 10) {
+        my_put_nbr(nb2 / 10);
+        my_putchar(nb2 % 10 + 48);
+    } else {
+        my_putchar(nb2 + 48);
+    }
+}
