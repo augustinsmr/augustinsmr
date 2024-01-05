e517df885decc76fb1748d2892ff514c498f5dfe
MathildeLekens <mathilde.lekens@epitech.eu>
2024-01-05 17:20:31 +0100

    [+] Added my_putchar.c : File used to display a character

---
+++ lib/my/my_putchar.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPE-110-LIL-1-1-secured-mathilde.lekens
+** File description:
+** my_putchar.c
+*/
+
+#include "../../include/hashtable.h"
+
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
