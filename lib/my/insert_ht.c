251fdbe915841f353a38a2d84d572933f6ff9f33
augustinsmr <augustin.simar@epitech.eu>
2024-01-07 14:33:26 +0100

    [+] Added insert_ht.c : File used to insert a key into the hashtable

---
+++ lib/my/insert_ht.c
@@ -0,0 +1,67 @@
+/*
+** EPITECH PROJECT, 2024
+** secured
+** File description:
+** insert_ht.c
+*/
+
+#include "../../include/hashtable.h"
+
+int retrieve_length(hashtable_t *ht)
+{
+    int i = 0;
+
+    while (ht[i].index != -1)
+        i ++;
+    return i;
+}
+
+void append_node(hashtable_t *ht, int key, char *value)
+{
+    hash_value_t *new = malloc(sizeof(hash_value_t));
+    int len_value = my_strlen(value);
+    hash_value_t *temp = ht->ht_values;
+
+    new->value = malloc(sizeof(char) * (len_value + 1));
+    my_strcpy(new->value, value);
+    new->key = key;
+    if (ht->ht_values == NULL) {
+        ht->ht_values = new;
+    } else {
+        new->next = temp;
+        ht->ht_values = new;
+    }
+}
+
+int store(int hashed_key, int len_ht)
+{
+    int temp = hashed_key;
+    int i = 0;
+    int boup = 1;
+
+    while (temp > 0) {
+        temp /= 10;
+        i ++;
+    }
+    temp = hashed_key;
+    for (int j = 0; j < i; j ++) {
+        temp /= 10;
+        if (j % 2 == 0 && temp % 10 != 0)
+            boup *= (temp % 10);
+    }
+    return (hashed_key - boup) % len_ht;
+}
+
+int ht_insert(hashtable_t *ht, char *key, char *value)
+{
+    int len_ht = retrieve_length(ht);
+    int hashed_key = ht->hash(key, len_ht);
+    int storing_place = store(hashed_key, len_ht);
+
+    for (int i = 0; i < len_ht; i ++) {
+        if (ht[i].index == storing_place) {
+            append_node(&(ht[i]), hashed_key, value);
+        }
+    }
+    return 0;
+}
