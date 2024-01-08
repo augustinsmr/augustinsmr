90bbf21cb086ea0e14044787e8fd29cd7cf2e6d1
MathildeLekens <mathilde.lekens@epitech.eu>
2024-01-08 13:00:47 +0100

    [+] Added delete_hashtable.c : File used to delete the hashtable

---
+++ lib/my/delete_hashtable.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPE-110-LIL-1-1-secured-mathilde.lekens
+** File description:
+** delete.c
+*/
+
+#include "../../include/hashtable.h"
+
+void delete_hashtable(hashtable_t *ht)
+{
+    int i = 0;
+    hash_value_t *tmp;
+    hash_value_t *tmp2;
+
+    while (ht[i].index != -1) {
+        tmp = ht[i].ht_values;
+        while (tmp != NULL) {
+            tmp2 = tmp->next;
+            free(tmp->value);
+            free(tmp);
+            tmp = tmp2;
+        }
+    }
+    free(tmp);
+    free(tmp2);
+    free(ht);
+}
